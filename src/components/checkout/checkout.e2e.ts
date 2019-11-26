98ec9ba788f184538d1758705b24c320fe2674c2
Mario <builtbysplash@gmail.com>
2019-11-26 21:37:03 +0100

    fix tests due to new theming

--- src/components/checkout/checkout.e2e.ts
+++ src/components/checkout/checkout.e2e.ts
@@ -9,27 +9,6 @@ describe('checkout', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('should have dark theme by default', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<nim-checkout></nim-checkout>');
-    const element = await page.find('nim-checkout >>> button');
-    expect(element).toHaveClass('nim-theme-dark');
-  });
-
-  it('should apply light theme', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<nim-checkout></nim-checkout>');
-    const element = await page.find('nim-checkout');
-    element.setProperty('theme', 'light');
-
-    await page.waitForChanges();
-
-    const button = await page.find('nim-checkout >>> button');
-    expect(button).toHaveClass('nim-theme-light');
-  });
-
   it('should open nimiq hub to proceed to checkout', async () => {
     const page = await newE2EPage();
 
